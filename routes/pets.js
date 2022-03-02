0a82babed1c503ed782f31a4274e757f520eb1eb
CHRIS_LEE <chrismlee87@gmail.com>
2022-03-02 14:20:59 -0800

    [add] search functionality for name and breed

--- routes/pets.js
+++ routes/pets.js
@@ -3,16 +3,15 @@ const Pet = require('../models/pet');
 
 // PET ROUTES
 module.exports = (app) => {
-
   // INDEX PET => index.js
 
   // NEW PET
-  app.get('/pets/new', (req, res) => {
-    res.render('pets-new');
+  app.get("/pets/new", (req, res) => {
+    res.render("pets-new");
   });
 
   // CREATE PET
-  app.post('/pets', (req, res) => {
+  app.post("/pets", (req, res) => {
     var pet = new Pet(req.body);
 
     pet.save()
@@ -21,28 +20,28 @@ module.exports = (app) => {
       })
       .catch((err) => {
         // Handle Errors
-      }) ;
+      });
   });
 
   // SHOW PET
-  app.get('/pets/:id', (req, res) => {
+  app.get("/pets/:id", (req, res) => {
     Pet.findById(req.params.id).exec((err, pet) => {
-      res.render('pets-show', { pet: pet });
+      res.render("pets-show", { pet: pet });
     });
   });
 
   // EDIT PET
-  app.get('/pets/:id/edit', (req, res) => {
+  app.get("/pets/:id/edit", (req, res) => {
     Pet.findById(req.params.id).exec((err, pet) => {
-      res.render('pets-edit', { pet: pet });
+      res.render("pets-edit", { pet: pet });
     });
   });
 
   // UPDATE PET
-  app.put('/pets/:id', (req, res) => {
+  app.put("/pets/:id", (req, res) => {
     Pet.findByIdAndUpdate(req.params.id, req.body)
       .then((pet) => {
-        res.redirect(`/pets/${pet._id}`)
+        res.redirect(`/pets/${pet._id}`);
       })
       .catch((err) => {
         // Handle Errors
@@ -50,9 +49,18 @@ module.exports = (app) => {
   });
 
   // DELETE PET
-  app.delete('/pets/:id', (req, res) => {
+  app.delete("/pets/:id", (req, res) => {
     Pet.findByIdAndRemove(req.params.id).exec((err, pet) => {
-      return res.redirect('/')
+      return res.redirect("/");
     });
   });
-}
+
+  // Search Pet
+  app.get("/search", (req, res) => {
+    searchTerm = new RegExp(req.query.term, "i");
+
+    Pet.find({ $or: [{ name: searchTerm }, { species: searchTerm }] }).exec((err, pets) => {
+      res.render("pets-index", { pets: pets });
+    });
+  });
+};
